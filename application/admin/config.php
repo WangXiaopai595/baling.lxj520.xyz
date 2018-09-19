6d5e592fe51ac5181e153489a4f29b7759130a81
lxj <864969742@qq.com>
2018-09-19 15:01:07 +0800

    微信配置

--- application/admin/config.php
+++ application/admin/config.php
@@ -9,4 +9,12 @@ return [
 	'view_replace_str'   =>  array(
 		'__STYLE__'         =>  '/static/admin'
 	),
+
+	'wx_config'		=> [
+		'key' => 'b998aedb3db8c40e89418556cd79d0d4',
+		'appid' => 'wx779b9fef5504815f',
+		'EncodingAESKey' => 'fvkDcwNM6IxrTmoLjksZVyH9uyKH34UZ5S4ozktIIaV',
+		'url' => 'http://baling.lxj520.xyz',
+		'token' => '2bce7b2ca7f6950f2c1500190145ff5a'
+	]
 ];
